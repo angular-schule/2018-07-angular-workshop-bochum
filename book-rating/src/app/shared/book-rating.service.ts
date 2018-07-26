ad0658280fe108b0d9a961506c71eae8695b3b60
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-07-26 12:53:34 +0200

    implements rating test

--- book-rating/src/app/shared/book-rating.service.ts
+++ book-rating/src/app/shared/book-rating.service.ts
@@ -6,11 +6,20 @@ import { Book } from './book';
 })
 export class BookRatingService {
 
+  private minRating = 1;
+  private maxRating = 5;
+
   rateUp(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: Math.min(this.maxRating, book.rating + 1)
+    };
   }
 
   rateDown(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: book.rating > this.minRating ? book.rating - 1 : this.minRating
+    };
   }
 }
